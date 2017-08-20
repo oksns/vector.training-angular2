6e1a209ddf52a318aee4826cfd0bbb95897df746
Оксана Седова <oksanasedova@MacBook-Pro-Oksana.local>
2017-08-20 22:20:44 +0300

    first commit

---
+++ src/app/materials/materials.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'materials',
+  templateUrl: './materials.component.html',
+})
+
+export class MaterialsComponent {
+
+}
---
+++ src/app/page-not-found/pageNotFound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-root',
+  templateUrl: './pageNotFound.component.html',
+  styleUrls: ['../app.component.scss'],
+
+
+})
+export class PageNotFoundComponent {
+
+}
