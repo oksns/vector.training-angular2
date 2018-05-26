a54c21e6d8053a1fdd8b2ae54ef21c80d329d2f6
Oksana Sedova <oksnsedova@gmail.com>
2018-05-26 19:36:06 +0300

    mocks updated

--- src/app/mock-usersRoles.ts
+++ src/app/mock-usersRoles.ts
@@ -4,11 +4,11 @@ export const USERS = [
     password: 'oks',
     name: 'Sedova Oksana',
     roles: ['student']
-  }
+  },
   {
     login: 'admin',
     password: 'admin',
     name: 'Admin Adminov',
     roles: ['admin']
   }
-]
+];
