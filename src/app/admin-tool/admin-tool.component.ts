77aa55a56f7b6dd8ae09b5b2d22b35eaedb2a919
dlobanov <lobanovdmitry777@gmail.com>
2018-05-29 00:35:10 +0300

    админка финита

--- src/app/admin-tool/admin-tool.component.ts
+++ src/app/admin-tool/admin-tool.component.ts
@@ -54,18 +54,44 @@ export class AdminToolComponent implements OnInit {
     let role = row[1];
     let component = componentsList[index-2];
     console.log(role, component, checked);
-    //снимаем роль, со всех детей
+    let allChild = [role];
+    for (let i = 0; i < allChild.length; i++) {
+      for (let role of this.rolesHelperService.rolesTree) {
+        if (allChild[i] === role.parent) allChild.push(role.name)
+      }
+    }
+    let allParents = [role];
+    for (let i = 0; i < allParents.length; i++) {
+      for (let role of this.rolesHelperService.rolesTree) {
+        if (allParents[i] === role.name) allParents.push(role.parent)
+      }
+    }
+
+    console.log(role + ' ' + allChild.toString() + ' ' + allParents.toString())
+
+    //снимаем роль, со всех детей, одеваем на всех родителей
     if (checked) {
-      let allChild = [role];
-      for (let i = 0; i < allChild.length; i++) {
-        for (let role of this.rolesHelperService.rolesTree) {
-          if (allChild[i] === role.parent) allChild.push(role.name)
+
+
+      let componentRoles = this.rolesHelperService.componentsRoles[component];
+      console.log(componentRoles.toString());
+      let componetRolesResult = componentRoles;
+
+
+      for (let role of allParents) {
+        let isNeedToAdd = true;
+        for (let roleFromCurrentList of componetRolesResult) {
+          if (roleFromCurrentList === role) {
+            isNeedToAdd = false;
+          }
+        }
+        if (isNeedToAdd) {
+          componetRolesResult.push(role);
         }
       }
 
-      let componentRoles = this.rolesHelperService.componentsRoles[component];
-      let componetRolesResult = [];
-      for (let role of componentRoles) {
+      let componetRolesResultWithoutChild = [];
+      for (let role of componetRolesResult) {
         let isNeedToRemove = false;
         for (let rolesToRemove of allChild) {
           if (role === rolesToRemove) {
@@ -73,33 +99,22 @@ export class AdminToolComponent implements OnInit {
           }
         }
 
-        if (!isNeedToRemove) componetRolesResult.push(role)
+        if (!isNeedToRemove) componetRolesResultWithoutChild.push(role)
       }
-      this.rolesHelperService.componentsRoles[component] = componetRolesResult;
+      console.log(componetRolesResultWithoutChild.toString());
+
+
+
+
+      this.rolesHelperService.componentsRoles[component] = componetRolesResultWithoutChild;
 
     }
 
+    role = row[1];
+    // когда ставим галочку, достаточно ее просто поставить :) остально само посчитается:)
     if (!checked) {
-      let allParents = [role];
-      for (let i = 0; i < allParents.length; i++) {
-        for (let role of this.rolesHelperService.rolesTree) {
-          if (allParents[i] === role.name) allParents.push(role.parent)
-        }
-      }
-
-      let componentRoles = this.rolesHelperService.componentsRoles[component];
-      let componetRolesResult = [];
-      for (let role of componentRoles) {
-        let isNeedToRemove = false;
-        for (let rolesToRemove of allParents) {
-          if (role === rolesToRemove) {
-            isNeedToRemove = true;
-          }
-        }
-
-        if (!isNeedToRemove) componetRolesResult.push(role)
-      }
-      this.rolesHelperService.componentsRoles[component] = componetRolesResult;
+      console.log(role);
+      this.rolesHelperService.componentsRoles[component].push(role);
 
     }
 
